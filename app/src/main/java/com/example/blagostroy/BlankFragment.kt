0702bd4317c879b43cdff6e262fb6db4466a0d90
bqk33 <downer1997@mail.ru>
2023-06-11 16:39:46 +0300

    RecyclerView in Fragment

--- app/src/main/java/com/example/blagostroy/BlankFragment.kt
+++ app/src/main/java/com/example/blagostroy/BlankFragment.kt
@@ -5,10 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.blagostroy.databinding.FragmentBlankBinding
 
 class BlankFragment : Fragment() {
     lateinit var binding: FragmentBlankBinding
+    //адаптер и список
+    private val adapter = PlantAdapter()
+    private val imageIdList = listOf(
+        R.drawable.tiles1,
+        R.drawable.tiles2,
+        R.drawable.tiles3,
+        R.drawable.tiles4,
+        R.drawable.tiles5)
+    private var index = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,10 +30,25 @@ class BlankFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        init()
     }
 
     companion object {
         @JvmStatic
         fun newInstance() = BlankFragment()
     }
+
+    private fun init(){
+        binding.apply {
+            rcView.layoutManager = GridLayoutManager(requireContext(), 2)
+            rcView.adapter = adapter
+            buttonTest.setOnClickListener {
+                if (index > 4) index = 0
+                val plant = Plant(imageIdList[index], "Tiles $index")
+                adapter.addPlant(plant)
+                index++
+            }
+        }
+    }
+
 }
